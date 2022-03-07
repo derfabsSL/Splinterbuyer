79371584b2b3b9a0094a3b535697e54766cc8572
derfabsSL <100463366+derfabsSL@users.noreply.github.com>
2022-03-07 08:31:18 +0100

    improved parsing, added option to buy with credits

--- Splinterbuyer.py
+++ Splinterbuyer.py
@@ -1,17 +1,28 @@
+from logging.handlers import RotatingFileHandler
 import os
 from beem.blockchain import Blockchain
 from beem import Hive
 import json
 import requests
+import logging 
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(format = Log_Format)
+logger = logging.getLogger()
+handler = RotatingFileHandler(os.path.join(THIS_FOLDER, 'transactions.log'), maxBytes=2000, backupCount=10)
+logger.addHandler(handler)
+logger.info("starting...")
 
 # Opening JSON file
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
 my_file = os.path.join(THIS_FOLDER, 'bids.json')
 f = open(my_file)
 configfile = json.load(f)
 bids = configfile["bids"]
 account_name = configfile["account"]["name"]
 active_key = configfile["account"]["active_key"]
+currency = configfile["account"]["currency"]
 f.close()
 
 hive = Hive(keys=active_key)
@@ -29,6 +40,8 @@ currently_buying = []
 def check_desired(listing, trx_id):
   price = float(listing["price"])
   cardid = str(listing["cards"])[5:-13]
+  if(len(cardid) > 5):
+    raise Exception("Invalid CardID")
   print(str(listing["cards"])[2] + "-" + cardid + " $" + str(price))
   for bid in bids:
     if (float(bid["max_quantity"]) > 0 
@@ -46,6 +59,7 @@ for bid in bids:
     bid["cards_tmp"] = [card for card in cardsjson if str(card["rarity"]) in bid["rarities"] and int(card["id"]) < 330]
   else:
     bid["cards_tmp"] = [card for card in cardsjson if str(card["rarity"]) in bid["rarities"]]
+  bid["cards"] = []
   for ed in bid["editions"]:
     current_ed = [str(card["id"]) for card in bid["cards_tmp"] if str(ed) in card["editions"]]
     bid["cards"] =  bid["cards"] + current_ed
@@ -57,19 +71,21 @@ for op in stream:
   if(op["type"] == 'custom_json'):
     if op["id"] == 'sm_sell_cards':
       try:
+        listings = []
         if(op["json"][:1] == '[' ):
-          listing = json.loads(op["json"][1:-1])
+          str_listings = op["json"].strip().replace(" ", "").replace("'", "")
+          listings = json.loads(str_listings)
         else:
-          listing = json.loads(op["json"])
-        
-        if (check_desired(listing, op["trx_id"]) == True):
-            price = float(listing["price"])
-            cardid = str(listing["cards"])[5:-13]
-            id = op["trx_id"]
-            jsondata_old = '{"items":["'+ str(id) + '-0"], "price":' + str(price) +', "currency":"DEC"}'
-            hive.custom_json('sm_market_purchase', json_data=jsondata_old, required_auths=[account_name])
-      except:
-        print("...") 
+          listings.append(json.loads(op["json"]))
+        for index, listing in enumerate(listings):
+          if (check_desired(listing, op["trx_id"]) == True):
+              price = float(listing["price"])
+              cardid = str(listing["cards"])[5:-13]
+              id = op["trx_id"]
+              jsondata_old = '{"items":["'+ str(id) + '-' + index + '"], "price":' + str(price) +', "currency":"' + str(currency) + '"}'
+              hive.custom_json('sm_market_purchase', json_data=jsondata_old, required_auths=[account_name])
+      except Exception as e:
+          logger.error("error occured while checking cards: "  + repr(e))
     else:
       if(len(currently_buying) > 0 and account_name in op["required_auths"]):
         try:
@@ -81,6 +97,8 @@ for op in stream:
             print("############################")
             print("successfully bought card for: " + str(res["total_dec"]) + "DEC")
             print("############################")
+            logger.info(res)
+
             for buy in currently_buying:
               if((str(buy["id"]) + "-0") in buydata["items"]):
                 currently_buying.remove(buy)
@@ -90,5 +108,5 @@ for op in stream:
                 bids[buy["bid_idx"]]["max_quantity"] = bids[buy["bid_idx"]]["max_quantity"] + 1
                 print("buy failed, card already sold")
                 currently_buying.remove(buy)
-        except:
-          print("...")
+        except Exception as e:
+          logger.error("error occured while buying: "  + repr(e))
